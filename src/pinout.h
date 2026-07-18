327508cf1c66e922587b363fe14abdff288b6c9d
Paul Mahlaichuk <mahlaichuk@gmail.com>
2026-07-18 20:21:55 +0300

    Commit 0002: Prepare board abstraction for Waveshare ESP32-S3 Touch LCD 2.8C

--- src/pinout.h
+++ src/pinout.h
@@ -1,10 +1,16 @@
 #ifndef PINOUT_H
 #define PINOUT_H
 
-#ifdef KNOMIV2
+#ifdef WAVESHARE28C
+
+#include "pinout_waveshare_28c.h"
+
+#elif defined(KNOMIV2)
+
 #include "pinout_knomi_v2.h"
+
 #else
+
 #include "pinout_knomi_v1.h"
-#endif
 
 #endif
---
+++ src/pinout_waveshare_28c.h
@@ -0,0 +1,10 @@
+#pragma once
+
+//
+// Waveshare ESP32-S3-Touch-LCD-2.8C
+// Temporary stub.
+// Real pin mapping will be added in Commit 0003.
+//
+
+#define TFT_WIDTH   480
+#define TFT_HEIGHT  480
